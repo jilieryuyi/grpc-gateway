f3de7a6b482c9104f4c0fbc29517aaa1505144a2
yuyi <297341015@qq.com>
2018-03-26 22:50:29 +0700

    base develop

--- service/resolver.go
+++ service/resolver.go
@@ -1,9 +1,7 @@
 package service
 
 import (
-	"errors"
-	"fmt"
-
+	log "github.com/sirupsen/logrus"
 	consul "github.com/hashicorp/consul/api"
 	"google.golang.org/grpc/naming"
 )
@@ -11,33 +9,33 @@ import (
 // ConsulResolver is the implementaion of grpc.naming.Resolver
 type ConsulResolver struct {
 	ServiceName string //service name
+	watch *ConsulWatcher
 }
 
 // NewResolver return ConsulResolver with service name
-func NewResolver(serviceName string) *ConsulResolver {
-	return &ConsulResolver{ServiceName: serviceName}
+func NewResolver(serviceName string, consulAddress string) *ConsulResolver {
+	if serviceName == "" {
+		log.Panicf("no service name provided")
+	}
+	res := &ConsulResolver{ServiceName: serviceName}
+	// generate consul client, return if error
+	conf := &consul.Config{
+		Scheme:  "http",
+		Address: consulAddress,
+	}
+	client, err := consul.NewClient(conf)
+	if err != nil {
+		log.Panicf("creat consul error: %v", err)
+	}
+	// return ConsulWatcher
+	res.watch = &ConsulWatcher{
+		cr: res,
+		cc: client,
+	}
+	return res
 }
 
 // Resolve to resolve the service from consul, target is the dial address of consul
 func (cr *ConsulResolver) Resolve(target string) (naming.Watcher, error) {
-	if cr.ServiceName == "" {
-		return nil, errors.New("wonaming: no service name provided")
-	}
-
-	// generate consul client, return if error
-	conf := &consul.Config{
-		Scheme:  "http",
-		Address: target,
-	}
-	client, err := consul.NewClient(conf)
-	if err != nil {
-		return nil, fmt.Errorf("wonaming: creat consul error: %v", err)
-	}
-
-	// return ConsulWatcher
-	watcher := &ConsulWatcher{
-		cr: cr,
-		cc: client,
-	}
-	return watcher, nil
+	return cr.watch, nil
 }
