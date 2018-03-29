fe9c0f5f773a68a84ccfd8aad30ff2a0d8dfb940
yuyi <297341015@qq.com>
2018-03-29 20:11:35 +0800

    base develop

--- grpc/client.go
+++ grpc/client.go
@@ -11,70 +11,15 @@ import (
 	"google.golang.org/grpc"
 	"github.com/jilieryuyi/grpc-gateway/proto"
 	"github.com/jilieryuyi/grpc-gateway/service"
-	"encoding/json"
-	//"google.golang.org/grpc/balancer/roundrobin"
 )
 
-
-
-func MyCodec() grpc.Codec {
-
-	return &myprotoCodec{}
-}
-
-type myprotoCodec struct{
-}
-
-
-// CodecWithParent returns a proxying grpc.Codec with a user provided codec as parent.
-//
-// This codec is *crucial* to the functioning of the proxy. It allows the proxy server to be oblivious
-// to the schema of the forwarded messages. It basically treats a gRPC message frame as raw bytes.
-// However, if the server handler, or the client caller are not proxy-internal functions it will fall back
-// to trying to decode the message using a fallback codec.
-//func MyCodecWithParent(fallback grpc.Codec) grpc.Codec {
-//	return &rawCodec{fallback}
-//}
-
-
-func (myprotoCodec) Marshal(v interface{}) ([]byte, error) {
-	fmt.Printf("myprotoCodec Marshal msg: %v\n", v)
-	//return []byte(v.([]byte)), nil
-	p2, _ := json.Marshal(v)
-	//jtype := []byte("----json----")
-	//buf := make([]byte, 0, len(jtype) + len(p2))
-	//buf = append(buf, jtype...)
-	//buf = append(buf, p2...)
-	return p2, nil
-}
-
-func (d myprotoCodec) Unmarshal(data []byte, v interface{}) error {
-	fmt.Printf("myprotoCodec Unmarshal msg: %v, ,,, %+v\n", string(data), data)
-	//return proto.Unmarshal(data, v.(proto.Message))
-	return json.Unmarshal(data, v)
-}
-
-func (myprotoCodec) String() string {
-	fmt.Println("myprotoCodec string")
-	return "proto"
-}
-
-func (myprotoCodec) Name() string {
-	fmt.Println("myprotoCodec string")
-	return "proto"
-}
-
-
 func main() {
-	//grpcAddr       := "http://127.0.0.1:8500"//"127.0.0.1:8081"
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), time.Second * 3)
+	opt    := grpc.WithDefaultCallOptions(grpc.CallCustomCodec(proto.Codec()))
+	r      := service.NewResolver()
+	b      := grpc.RoundRobin(r)
+	lb     := grpc.WithBalancer(b)
 
-	opt := grpc.WithDefaultCallOptions(grpc.CallCustomCodec(MyCodec()))
-
-	//balance := grpc.WithBalancerName(roundrobin.Name)
-	r  := service.NewResolver()
-	b  := grpc.RoundRobin(r)
-	lb := grpc.WithBalancer(b)
 	conn, err := grpc.DialContext(ctx, "service.add", grpc.WithInsecure(), opt, lb)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v", err)
